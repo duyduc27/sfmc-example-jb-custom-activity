4890c4851e7618f092dedb804f61cdfcfd731477
Duc Le <duyduc100@gmail.com>
2024-06-19 14:50:52 +0700

    Update config-json.js

--- modules/discount-redemption-split/config/config-json.js
+++ modules/discount-redemption-split/config/config-json.js
@@ -24,21 +24,21 @@ module.exports = function configJSON(req) {
         outArguments: [],
         // Fill in the host with the host that this is running on.
         // It must run under HTTPS
-        url: `https://${req.headers.host}/modules/discount-redemption-split/execute`
+        url: `https://dcle-test2-dd15b6793059/modules/discount-redemption-split/execute`
       }
     },
     configurationArguments: {
       save: {
-        url: `https://${req.headers.host}/modules/discount-redemption-split/save`
+        url: `https://dcle-test2-dd15b6793059/modules/discount-redemption-split/save`
       },
       publish: {
-        url: `https://${req.headers.host}/modules/discount-redemption-split/publish`
+        url: `https://dcle-test2-dd15b6793059/modules/discount-redemption-split/publish`
       },
       validate: {
-        url: `https://${req.headers.host}/modules/discount-redemption-split/validate`
+        url: `https://dcle-test2-dd15b6793059/modules/discount-redemption-split/validate`
       },
       stop: {
-        url: `https://${req.headers.host}/modules/discount-redemption-split/stop`
+        url: `https://dcle-test2-dd15b6793059/modules/discount-redemption-split/stop`
       }
     },
     userInterfaces: {
